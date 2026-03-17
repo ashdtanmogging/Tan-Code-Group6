141303e1ff903a16e54722b8f88ef94bcf1833be
adrenmogging <ashdtan@cmc.pshs.edu.ph>
2026-03-18 07:56:11 +0800

    Updated main.py and commited something bro

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 name = input("Name? ")
 print("Hello " + name + ".")
+
 age = input("Age? ")
 addr = input("City? ")
 print(name + " lives in " + addr)
+
 year = 2026 - int(age)
-print("Born in: " = str(year))
+print("Born in: ", str(year))
